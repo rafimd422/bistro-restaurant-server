4e9d0b57fbdda553cb48949e9f095782585b7671
rafimd422 <rafimf2222@gmail.com>
2023-11-17 03:14:26 +0600

    fixed a silly mistake where i was wrote get instead of delete on the delete request

--- index.js
+++ index.js
@@ -61,12 +61,13 @@ app.get('/carts', async(req, res) => {
   const result = await cursor.toArray()
   res.send(result)
 })
-// app.get('/carts/:id', async(req, res) => {
-//   const id = req.params.id
-//   const query = {_id: new ObjectId(id)}
-//   const result = await cartDB.deleteOne(query)
-//   res.send(result)
-// })
+
+ app.delete('/carts/:id', async(req, res) => {
+   const id = req.params.id
+   const query = {_id: new ObjectId(id)}
+   const result = await cartDB.deleteOne(query)
+   res.send(result)
+ })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
