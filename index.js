7da28871fcbd0184bc7cb170dee77bc53afec099
rafimd422 <rafimf2222@gmail.com>
2023-11-17 02:45:19 +0600

    created a delete api to give the freedom to user to delete item from cart

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.sopxnju.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,7 +52,15 @@ app.post('/carts', async(req, res)=> {
 })
 
 app.get('/carts', async(req, res) => {
-  const result = await cartDB.find().toArray()
+  const email = req.query.email
+  const query = {email:email}
+  const result = await cartDB.find(query).toArray()
+  res.send(result)
+})
+app.get('/carts/:id', async(req, res) => {
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const result = await cartDB.deleteOne(query)
   res.send(result)
 })
     await client.db("admin").command({ ping: 1 });
